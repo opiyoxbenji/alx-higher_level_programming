0abe40de3b0684333260ad01ddba64ddd6734953
pixelBushi <opiyosbenji@gmail.com>
2023-09-07 17:54:28 +0300

    Import the function add(a, b) from the file add_0.py and print the result of adding 1 and 2, which should be 3.

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    result = a + b
-    print(f"{a} + {b} = {result}")
-    return result
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
