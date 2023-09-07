e3b61281dd4fc0e1901d9ed81a6b62ce2e059e09
pixelBushi <opiyosbenji@gmail.com>
2023-09-07 14:34:37 +0300

    deletions

--- 0x02-python-import_modules/0-add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-
-result = add_0.add(a, b)
-
-print("{:d} + {:d} = {:d}".format(a, b, result))
--- 0x02-python-import_modules/add_0.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def add(a, b):
-    result = a + b
-    print(f"{a} + {b} = {result}")
-    return result
