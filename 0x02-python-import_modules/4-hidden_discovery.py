bd3493e253eb44fac66743a1198ff51a3cf33fab
pixelBushi <opiyosbenji@gmail.com>
2023-09-07 23:41:44 +0300

    a Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    sorted_name = (dir(hidden_4))
-    for name in sorted_name:
-        if not name.startswith("__"):
-            print("{:s}".format(name))
+
+    sorted_ = dir(hidden_4)
+    for name in sorted_:
+        if name[:2] != "__":
+            print(name)
