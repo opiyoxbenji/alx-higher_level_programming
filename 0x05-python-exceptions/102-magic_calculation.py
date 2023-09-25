9ee646d724cb061d2418352e6ab608139f952dcd
opiyoxbenji <opiyosbenji@gmail.com>
2023-09-25 16:35:21 +0300

    Python function def magic_calculation(a, b): that does exactly the same

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+
+    for arr in range(1, 3):
+        try:
+            if arr > a:
+                raise Exception("Too far")
+            result += (a, b) / arr
+        except Exception as e:
+            result = b + a
+            break
+
+    return result
