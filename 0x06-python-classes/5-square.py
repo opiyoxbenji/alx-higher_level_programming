91f9bddd9e5b1928313a0110917342ea64419112
opiyoxbenji <opiyosbenji@gmail.com>
2023-09-27 00:05:36 +0300

    updates

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,62 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bib/python3
 """
-Module 3
-Defines Class square
-"""
-
-
-class Square:
-    """
-    class square definition
-    """
-
-    def __init__(self, size=0):
-        """
-        Initiliasation of square
-        """
-        self.size = size
-
-    @property
-    def size(self):
-        """
-        retrieves current size
-        """
-        ar = self.__size
-        return ar
-
-    @size.setter
-    def size(self, value):
-        """
-        raises all errors
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        """
-        returns current square area
-        """
-        arr = self.__size ** 2
-        return arr
-
-    def my_print(self):
-        """
-        prints with # to stdout
-        """
-        if self.__size == 0:
-            print()
-        else:
-            for row in range(self.__size):
-                print("#" * self.__size)  # !/usr/bin/python3
-
-
-"""
-Module 3
-Defines Class square
+Defines a Class square
 """
 
 
