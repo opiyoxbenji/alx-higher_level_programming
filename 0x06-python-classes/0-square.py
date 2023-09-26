5a0e839280469d5b226326ee04f10f6e956a7c52
opiyoxbenji <opiyosbenji@gmail.com>
2023-09-26 19:59:24 +0300

    x

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
+"""Defines a square."""
+
 
 class Square:
-    """
-    This is the Square class.
-    It represents a square and provides a method to calculate its area.
-    """
-    
-    def square_area(self, side_len):
-        """
-        Calculate the area of the square.
-
-        :param side_len: Length of one side of the square.
-        :return: The area of the square.
-        """
-        self.side_len = side_len
-        area = self.side_len ** 2
-        return area
+    """Where the square goes."""
+    pass
