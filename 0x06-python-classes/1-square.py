03c567aa85d06cfac366c9414a0b45d130d69879
opiyoxbenji <opiyosbenji@gmail.com>
2023-09-26 20:03:33 +0300

    lilb3nji

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """This is a square class."""
+    
+    def __init__(self, size):
+        """Initialize a new square instance"""
+        
+        self.__size = size
