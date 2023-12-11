e05d7089c1bbaf350b08f4ea88f315b80d34f7c9
root <opiyosbenji@gmail.com>
2023-12-11 11:32:24 -0800

    prints adtion of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -7,9 +7,5 @@ function add (a, b) {
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('NaN');
-} else {
-  const result = add(num1, num2);
-  console.log(`The sum of ${num1} and ${num2} is: ${result}`);
-}
+const result = add(num1, num2);
+console.log(`The sum of ${num1} and ${num2} is: ${result}`);
