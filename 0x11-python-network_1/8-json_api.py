e60e6bb71fffc6bd2b4a8c57a019b11a94bede1e
pixelbushi <opiyosbenji@gmail.com>
2024-01-07 23:27:42 +0300

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter."""
+"""POSTs request to http://0.0.0.0:5000/search_user"""
 import sys
 import requests
 
