2ea2540fafbe0e83f572e154c9fed545051a046c
opiyoxbenji <opiyosbenji@gmail.com>
2024-01-30 10:52:58 +0000

    reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, result) => {
-	err ? console.log(err) : console.log(result);
+  err ? console.log(err) : console.log(result);
 });
