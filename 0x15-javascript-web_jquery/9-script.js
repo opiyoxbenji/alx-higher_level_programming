d95ad3cc611fe6b620de785c4317892364118817
opiyoxbenji <opiyosbenji@gmail.com>
2024-02-07 12:40:17 +0000

    update

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,12 +1,3 @@
-$(document).ready(function () {
-  $.ajax({
-    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-    dataType: 'json',
-    success: function (data) {
-      $('#hello').text(data.hello);
-    },
-    error: function () {
-      console.error('Failed to fetch translation.');
-    }
-  });
+$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+	$('DIV#hello').text(data.hello);
 });
